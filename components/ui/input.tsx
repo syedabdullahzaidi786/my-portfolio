05d0230bfa40495cb981c48511c4383cafcf2617
Syed Abdullah Zaidi <160156087+syedabdullahzaidi786@users.noreply.github.com>
2024-11-06 23:24:11 +0500

    Update input.tsx

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-<<<<<<< HEAD
 const Input: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = ({ className = "", ...props }) => {
   return (
     <input
@@ -10,19 +9,4 @@ const Input: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = ({ classNam
   );
 };
 
-=======
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  // You can add custom props here if needed
-}
-
-const Input: React.FC<InputProps> = ({ className, ...props }) => {
-  return (
-    <input
-      {...props}
-      className={`bg-gray-700 border-gray-600 text-gray-100 placeholder-gray-400 rounded-lg p-3 w-full ${className}`}
-    />
-  );
-};
-
->>>>>>> c3bb9e7f2d65651c9bbf1c7867530795cafae985
 export default Input;
